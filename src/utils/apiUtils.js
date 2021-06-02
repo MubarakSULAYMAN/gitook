8659a5eb57577d0cea75a9b8ff7d782b2766e7a0
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2021-06-02 07:46:30 +0100

    improved token fetching

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,32 +1,12 @@
 import axios from 'axios'
 
 const apiRequest = axios.create({
-    // https://api.github.com/search/code?q={query}{&page,per_page,sort,order}
-    // "https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
-    // baseURL: `https://api.github.com/search/`,
-    // baseURL: `https://api.github.com/search/users?q=Mubarak&per_page=10&page=1&sort=undefined&order=undefined`,
     baseURL: `https://api.github.com`,
-    // baseURL: `https://api.github.com/`,
     withCredentials: false,
     headers: {
         'Accept': 'application/vnd.github.v3+json',
         'Content-Type': 'application/json',
-        // 'Authorization': 'token 035f48430992d2bf7baf91594149ac9941b41fe9',
-        'Authorization': 'token e8374e40dea10ba1d5bebfa8846ca88a6184a17c',
-        // 'User-Agent':  'Demo',
-        // 'Client ID': 'd819e4179f776ca84404',
-        // 'Client secrets': '386a0f4a0b1aaf46deba4c5554f0b0970eccaa16',
-        // 'Owner': 'Mubarak SULAYMAN',
-        // 'Access-Control-Allow-Methods': 'GET',
-        // 'Access-Control-Allow-Origin': 'http://localhost:8080',
-        // 'Access-Control-Allow-Origin': 'http://gitook.vercel.app/',
-        // "Allow": 'HEAD, OPTIONS, GET'
-
-
-        //   headers: {
-          // "Accept: application/vnd.github.v3+json",
-        //     'Authorization': `token ${state.access_token}`
-        //   }
+      'Authorization': process.env.VUE_APP_KEY,
     }
 })
 
